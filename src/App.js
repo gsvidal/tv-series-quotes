4ee249cbbe2af6c3649aa15b0dd6e20f8383b096
Gonzo <gvidal.industrial@gmail.com>
2021-11-21 10:06:19 -0500

    Disable smallville button till API is ready and deploy settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import Selector from './components/Selector';
+import btnImgSmallville from './assets/smallville.png';
+import btnImgBreakingBad from './assets/breakingbad.png';
 
 const Container = styled.div`
   display: flex;
@@ -23,15 +25,31 @@ const ContainerMain = styled.div`
   }
 `;
 const Button = styled.button`
+  width: 280px;
+  height: 200px;
   padding: 1rem;
   margin: 1rem;
   font-size: 2rem;
-  background-color: #cfc;
   border: 1px solid #ccc;
   border-radius: 6px;
   color: black;
+  cursor: pointer;
+  opacity: 0.5;
+  transition: transform .1s linear;
   &:hover {
-    background-color: #aca;
+    opacity: 1;
+    transform: scale(1.05);
+  }
+  &.button__smallville {
+    background: url(${btnImgSmallville}) center/cover no-repeat;
+    &.disabled {
+      opacity: 0.2;
+      transform: scale(1);
+      cursor: initial;
+    }
+  }
+  &.button__breaking-bad {
+    background: url(${btnImgBreakingBad}) center/cover no-repeat;
   }
 `;
 
@@ -57,14 +75,16 @@ function App() {
         <h1>TV SERIES QUOTES</h1>
         <h2>Select your favourite series to get Quotes!</h2>
         <Button
-          onClick={handleClick}
+          // onClick={handleClick}
           value="smallville"
-        >Smallville
+          className="button__smallville disabled"
+        >
         </Button>
         <Button
           onClick={handleClick}
           value="breakingbad"
-        >Breaking bad
+          className="button__breaking-bad"
+        >
         </Button>
         
       </ContainerMain>
